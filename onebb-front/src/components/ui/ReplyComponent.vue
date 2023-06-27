b28403d7db9d20556864ce793b344f9875d3897d
PanKrok <mr.pankrok@gmail.com>
2023-06-27 13:38:42 +0200

    0000168

--- onebb-front/src/components/ui/ReplyComponent.vue
+++ onebb-front/src/components/ui/ReplyComponent.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import JoditComponent from '../JoditComponent.vue';
+import { useUser } from '@/hooks/useUser';
+import useApi from '@/hooks/useApi';
+import { useRoute } from 'vue-router';
 
+
+const {callback} = defineProps<{callback: Function}>();
+const route = useRoute();
+const {isLogged} = useUser();
+const {post} = useApi();
 const textValue = ref()
-const sendReply = () => {
-    console.log(textValue.value)
+const sendReply = async () => {
+    if (!isLogged()) {
+        return;
+    }
+
+    const response = await post('posts', {
+        plot: `/api/plots/${route.params.id}`,
+        content: textValue.value,
+    })
+
+    callback(response.parsedResponse ?? undefined)
 }
 </script>
 <template>
-    <div>
+    <div v-if="isLogged()">
         <JoditComponent :value="textValue" @update-event="(e)=>{textValue = e}"/>
         <div class="row margin-m justify-content-flex-end">
             <button class="button button-background-blue button-color-white border-radius-5" @click="sendReply()">
--- onebb-front/src/views/PlotView.vue
+++ onebb-front/src/views/PlotView.vue
@@ -7,53 +7,55 @@ import type { IPost, IPlot } from '@/interfaces/OnebbInterfaces';
 import PostComponent from '@/components/ui/PostComponent.vue';
 import PaginatorComponent from '@/components/PaginatorComponent.vue';
 import ReplyComponent from '@/components/ui/ReplyComponent.vue';
+import { useUser } from '@/hooks/useUser';
 
-const boxStyles = ['background-blue', 'border-radius-5', 'color-white', 'font-size-18', 'font-weight-500', 'padding-l', 'margin-l'];
+const { isLogged } = useUser();
+const boxStyles = ['background-blue', 'border-radius-5', 'color-white', 'font-size-18', 'font-weight-500', 'padding-l'];
 const route = useRoute();
+const cb = (addedPost: IPost | undefined) => {
+    if (typeof addedPost === 'undefined') {
+        return;
+    }
+
+    posts.value?.push(addedPost)
+}
 const plot = ref<IPlot>();
 const posts = ref<IPost[]>();
 const next = ref(false);
 const reply = ref(false);
 
 usePlot(route.params.id, route.params.page ?? 1).then(response => {
-    console.log({debugPlotResponse: response})
     const { plotResponse, postsResponse } = response;
     plot.value = plotResponse;
     if (postsResponse) {
         posts.value = postsResponse['hydra:member'];
         // @ts-ignore
         next.value = postsResponse['hydra:next'] ? true : false
-        
+
     }
 })
 
 </script>
 <template>
-    <TransitionGroup tag="div" name="list" class="column position-relative" mode="in-out" v-if="plot" >
+    <TransitionGroup tag="div" name="list" class="column position-relative margin-x-l" mode="in-out" v-if="plot">
         <Box :boxClass="boxStyles" key="plot-header">
             {{ plot.name }}
         </Box>
-        
+
         <Box v-for="post in posts" :key="post.id">
-           <PostComponent :post="post" />
+            <PostComponent :post="post" />
         </Box>
-        
-        <button 
-            key="reply-btn"
-            v-if="!reply"
-            class="button button-background-green button-color-white border-radius-5"
-            @click="reply = !reply"  
-        >
-            Reply
-        </button>
-        
-        <Box v-if="reply" key="reply">
-            <ReplyComponent />
-        </Box>
-        
+        <div class="row justify-content-flex-end" v-if="isLogged()">
+            <button key="reply-btn" v-if="!reply" class="button button-background-green button-color-white border-radius-5"
+                @click="reply = !reply">
+                Reply
+            </button>
 
-        <PaginatorComponent :next="next"/>
+            <Box :wrapperClass="['col-12']" v-if="reply" key="reply">
+                <ReplyComponent :callback="cb" />
+            </Box>
+        </div>
+
+        <PaginatorComponent :next="next" />
     </TransitionGroup>
-
-
 </template>
