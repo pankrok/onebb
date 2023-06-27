5c44e5736761f0baa7474ffe42fa7fe850938c9d
PanKrok <mr.pankrok@gmail.com>
2023-06-27 13:01:45 +0200

    0000167

--- onebb-front/src/components/ui/modals/RegisterModalComponent.vue
+++ onebb-front/src/components/ui/modals/RegisterModalComponent.vue
@@ -12,6 +12,13 @@ const creditionals = reactive<IRegister>({
     password: '',
     email: ''
 });
+
+const errors = reactive({
+    username: '',
+    password: '',
+    email: '',
+    vpassword: ''
+});
 const msg = reactive<{
     show: boolean,
     msg: string,
@@ -22,11 +29,16 @@ const msg = reactive<{
     class: []
 })
 const { register } = useUser();
-
+const vpassword = ref('')
 const signUp = async () => {
+    if (vpassword.value !== creditionals.password) {
+        errors.password = errors.vpassword = 'Passwords does not match!';
+        return;
+    }
+
     const isRegister = await register(creditionals);
-    
-    if (typeof isRegister?.parsedResponse.violations === undefined) {
+    // @ts-ignore
+    if (typeof isRegister.parsedResponse.violations === 'undefined') {
         msg.show = true;
         msg.msg = 'logged';
         msg.class = ['background-green', 'box-shadow-green'];
@@ -36,6 +48,12 @@ const signUp = async () => {
         }, 3000)
         return;
     }
+
+    // @ts-ignore
+    isRegister.parsedResponse.violations.forEach(err => {
+        // @ts-ignore
+        errors[err.propertyPath] = err.message;
+    })
 
     msg.show = true;
     msg.msg = 'Invalid creditionals!';
@@ -49,11 +67,11 @@ const signUp = async () => {
 </script>
 <template>
     <Modal>
-        <BoxComponent :header="true">
+        <BoxComponent :wrapperClass="['minWidth-50wv']" :header="true">
             <template v-slot:header>
                 <div class="row jutify-content-space-around align-items-center margin-m">
                     <div class="font-size-18 font-weight-600 col-11">
-                        Login
+                        Register
                     </div>
                     <div class="col-1 cursor-pointer" @click="toggleModal()">
                         <svg xmlns="http://www.w3.org/2000/svg" height="1em"
@@ -64,7 +82,7 @@ const signUp = async () => {
                     </div>
                 </div>
             </template>
-            <div class="column padding-xl background-color-white">
+            <div class="column padding-xl background-color-white" style="min-width: 50wv;">
                 <Transition name="fade" mode="in-out">
                     <div class="position-relative" key="signUp-box">
                         <div v-if="msg.show"
@@ -76,11 +94,17 @@ const signUp = async () => {
                     </div>
                 </Transition>
                 <label class="label" for="username">Username</label>
-                <input class="form-control" id="username" type="text" v-model="creditionals.username" />
-                <label class="label" for="Email">Email</label>
+                <input class="form-control" :class="[errors.username.length > 0 ? 'border-color-red' : '']" id="username" type="text" v-model="creditionals.username" />
+                <span class="color-red font-size-12 margin-x-m">{{ errors.username }}</span>
+                <label class="label" :class="[errors.email.length > 0 ? 'border-color-red' : '']" for="Email">Email</label>
                 <input class="form-control" id="Email" type="text" v-model="creditionals.email" />
+                <span class="color-red font-size-12 margin-x-m">{{ errors.email }}</span>
                 <label class="label" for="password">Password</label>
-                <input class="form-control" id="password" type="password" v-model="creditionals.password" />
+                <input class="form-control" :class="[errors.password.length > 0 ? 'border-color-red' : '']" id="password" type="password" v-model="creditionals.password" />
+                <span class="color-red font-size-12 margin-x-m">{{ errors.password }}</span>
+                <label class="label" for="vpassword">Confirm password</label>
+                <input class="form-control" :class="[errors.vpassword.length > 0 ? 'border-color-red' : '']" id="vpassword" type="password" v-model="vpassword" />
+                <span class="color-red font-size-12 margin-x-m">{{ errors.vpassword }}</span>
                 <button class="button" @click="signUp()">Sign up</button>
             </div>
         </BoxComponent>
