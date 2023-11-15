3c4040daf5c363a474f9bbcc1b00e86fcfadbd9c
PanKrok <mr.pankrok@gmail.com>
2023-11-15 12:07:25 +0100

    fixed refreshing token

--- onebb-front/src/hooks/useAxios.ts
+++ onebb-front/src/hooks/useAxios.ts
@@ -1,27 +1,28 @@
 //import type { ITokenResponse } from '@/interfaces'
 import axios, { type AxiosResponse } from 'axios'
-import useLoadingStore from '@/stores/useLoadingStore';
+import useLoadingStore from '@/stores/useLoadingStore'
 import type { AxiosError } from 'axios'
-import type { ITokenResponse } from '@/interfaces';
-import { instanceOf } from './helpers';
-import useUserStore from '@/stores/useUserStore';
-import { REFRESH_URL } from '@/helpers/api';
+import type { ITokenResponse } from '@/interfaces'
+import { instanceOf } from './helpers'
+import useUserStore from '@/stores/useUserStore'
+import { REFRESH_URL } from '@/helpers/api'
 
-let numberOfAjaxCAllPending = 0;
-console.log({axiosEnv: import.meta.env.MODE})
+let numberOfAjaxCAllPending = 0
+
 const instance = axios.create({
   withCredentials: true,
   baseURL: import.meta.env.VITE_BASE_URL_API,
-  timeout: import.meta.env.MODE === 'development' ? 30000 : 3000
+  timeout: import.meta.env.MODE === 'development' ? 30000 : 3000,
+  
 })
 
 export default function useAxios() {
-  const loadingStore = useLoadingStore();
+  const loadingStore = useLoadingStore()
   instance.interceptors.request.use(
     function (config) {
       console.log('withCredentials ', instance.defaults.withCredentials)
       numberOfAjaxCAllPending++
-      loadingStore.isLoading();
+      loadingStore.isLoading()
       return config
     },
     function (error) {
@@ -33,38 +34,47 @@ export default function useAxios() {
     (response) => {
       numberOfAjaxCAllPending--
       if (numberOfAjaxCAllPending == 0) {
-        loadingStore.isLoaded();
+        loadingStore.isLoaded()
       }
       return response
     },
     async (error: AxiosError) => {
       numberOfAjaxCAllPending--
       if (numberOfAjaxCAllPending == 0) {
-        loadingStore.isLoaded();
+        loadingStore.isLoaded()
       }
-      console.log('withCredentials ', instance.defaults.withCredentials)
-      const {response} = error;
-      // @ts-ignore
-       if (response && response.data && response.data && response.status === 401 && response.data.message === 'Expired JWT Token') {
-        if(instance.defaults.headers.common['Authorization']) {
-          const data = await instance.post<{}, AxiosResponse<unknown>>(REFRESH_URL, {});
-              if (instanceOf<ITokenResponse>(data)) {
-                  const userStore = useUserStore();
-                  userStore.setUserData(data);
-                  const newResponse = instance.request(error.request);
-                  return Promise.resolve(newResponse)
-              }
+      console.log('err ', { error })
+      const { response } = error
+
+      if (
+        response &&
+        response.data &&
+        response.data &&
+        response.status === 401 && // @ts-ignore
+        response.data.message === 'Expired JWT Token'
+      ) {
+        if (instance.defaults.headers.common['Authorization']) {
+          const {data} = await instance.post<{}, AxiosResponse<unknown>>(REFRESH_URL, {})
+          if (instanceOf<ITokenResponse>(data)) {
+            const userStore = useUserStore()
+            userStore.setUserData(data)
+            
+            if (error.config) {
+              error.config.headers.Authorization = `Bearer ${data.token}`;
+              return instance.request(error.config)
+            }
           }
-       }
-      
+        }
+      }
+
       return Promise.reject(error)
     }
   )
 
   function setToken(token: string) {
-    console.log('setting token');
-    instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    console.log({axiosHeaders: instance.defaults.headers})
+    console.log('setting token')
+    instance.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    console.log({ axiosHeaders: instance.defaults.headers })
   }
 
   function removeToken() {
@@ -81,7 +91,7 @@ export default function useAxios() {
   return {
     setToken,
     removeToken,
-  //  refreshToken,
+    //  refreshToken,
     axios: instance
   }
 }
